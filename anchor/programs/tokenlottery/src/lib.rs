b00d5f5ae601aceeacd1ce8114267b5a702211e6
rishavmehra <rishavmehra61@gmail.com>
2025-01-13 01:27:12 +0530

    InitializeLottery accounts, Todo: func for this account

--- anchor/programs/tokenlottery/src/lib.rs
+++ anchor/programs/tokenlottery/src/lib.rs
@@ -1,9 +1,14 @@
-#![allow(clippy::result_large_err)]
-
 use anchor_lang::prelude::*;
-// 
+use anchor_spl::{
+  associated_token::AssociatedToken,
+  token_interface::{TokenInterface, Mint, TokenAccount}
+};
+use anchor_spl::metadata::{
+  Metadata,
+};
+
 declare_id!("C2Hh76cyFRXzAezpSp4VFMeeWVzGtB4g5zEmYUggkYcM");
-// coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF
+
 #[program]
 pub mod tokenlottery {
   use super::*;
@@ -17,12 +22,17 @@ pub mod tokenlottery {
     ctx.accounts.token_lottery.bump = ctx.bumps.token_lottery;
     ctx.accounts.token_lottery.start_time = start;
     ctx.accounts.token_lottery.end_time = end;
-    ctx.accounts.token_lottery.total_price = price;
+    ctx.accounts.token_lottery.ticket_price = price;
     ctx.accounts.token_lottery.authority = *ctx.accounts.payer.key;
     ctx.accounts.token_lottery.lottery_pot_amount = 0;
-    ctx.accounts.token_lottery.total_tickets = 0;
     ctx.accounts.token_lottery.randomness_account = Pubkey::default();
     ctx.accounts.token_lottery.winner_chosen = false;
+    Ok(())
+  }
+
+  pub fn initialize_lottery(
+    _ctx: Context<InitializeLottery>,
+  ) ->Result<()> {
     Ok(())
   }
 }
@@ -42,7 +52,66 @@ pub struct Initialize<'info> {
   pub token_lottery: Account<'info, TokenLottery>,
 
   pub system_program: Program<'info, System>,
+}
 
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+    init,
+    payer = payer,
+    mint::decimals=0,
+    mint::authority=collection_mint,
+    mint::freeze_authority=collection_mint,
+    seeds=[b"collection_mint".as_ref()],
+    bump
+  )]
+  pub collection_mint: InterfaceAccount<'info, Mint>,
+
+  #[account(
+    mut,
+    seeds=[
+      b"metadata",
+      token_metadata_program.key().as_ref(),
+      collection_mint.key().as_ref()
+    ],
+    bump,
+    seeds::program = token_metadata_program.key(),
+  )]
+  /// CHECK: This account check by the metadata smart contract
+  pub metadata: UncheckedAccount<'info>,
+
+  #[account(
+    mut,
+    seeds=[
+      b"metadata",
+      token_metadata_program.key().as_ref(),
+      collection_mint.key().as_ref(),
+      b"edition"
+      ],
+    bump,
+    seeds::program = token_metadata_program.key(),
+  )]
+  /// CHECK: This account check by the metadata smart contract
+  pub master_edition: UncheckedAccount<'info>,
+
+
+  #[account(
+    init,
+    payer = payer,
+    seeds = [b"collection_token_account".as_ref()],
+    bump,
+    token::mint = collection_mint,
+    token::authority = collection_token_account
+  )]
+  pub collection_token_account: InterfaceAccount<'info, TokenAccount>,
+
+  pub token_metadata_program: Program<'info, Metadata>,
+  pub associated_token_program: Interface<'info, AssociatedToken>,
+  pub token_program: Interface<'info, TokenInterface>,
+  pub system_program: Program<'info, System>,
 }
 
 #[account]
@@ -54,7 +123,7 @@ pub struct TokenLottery{
   pub end_time: u64,
   pub lottery_pot_amount: u64,
   pub total_tickets: u64,
-  pub total_price: u64,
+  pub ticket_price: u64,
   pub authority: Pubkey,
   pub randomness_account: Pubkey,
   pub bump: u8,
