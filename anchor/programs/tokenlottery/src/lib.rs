31775db461feaa853d7239ffa9bdee5d2a3b8a4c
rishavmehra <rishavmehra61@gmail.com>
2025-01-14 15:07:56 +0530

    buy ticket account completed | todo: func buy_ticket

--- anchor/programs/tokenlottery/src/lib.rs
+++ anchor/programs/tokenlottery/src/lib.rs
@@ -30,7 +30,6 @@ pub const URI: &str= "https://raw.githubusercontent.com/rishavmehra/token-lotter
 
 #[program]
 pub mod tokenlottery {
-use anchor_spl::metadata::{sign_metadata, SignMetadata};
 
 use super::*;
 
@@ -141,6 +140,10 @@ use super::*;
 
     Ok(())
   }
+
+  pub fn buy_ticket(_ctx: Context<BuyTicket>)-> Result<()>{
+    ok(())
+  }
 }
 
 #[derive(Accounts)]
@@ -219,6 +222,83 @@ pub struct InitializeLottery<'info> {
   pub token_program: Interface<'info, TokenInterface>,
   pub system_program: Program<'info, System>,
   pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct BuyTicket<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds= [b"token_lottery".as_ref()],
+    bump= token_lottery.bump,
+  )]
+  pub token_lottery: Account<'info, TokenLottery>,
+
+  #[account(
+      init,
+      payer = payer,
+      seeds= [token_lottery.total_tickets.to_le_bytes().as_ref()],
+      bump,
+      mint::decimals=0,
+      mint::authority=collection_mint,
+      mint::freeze_authority=collection_mint,
+      mint::token_program=token_program,
+  )]
+  pub ticket_mint: Interface<'info, Mint>,
+
+  #[account(
+    mut,
+    seeds=[
+      b"collection_mint".as_ref()
+    ],
+    bump
+  )]
+  pub collection_mint: InterfaceAccount<'info, Mint>,
+
+  #[account(
+    mut,
+    seeds= [
+      b"metadata",
+      token_metadata_program.key().as_ref(),
+      ticket_mint.key().as_ref(),
+    ],
+    bump,
+    seeds::program= token_metadata_program.key(),
+  )]
+  /// CHECK: This account check by the metadata smart contract
+  pub ticket_metadata: UncheckedAccount<'info>,
+
+  #[account(
+    mut, 
+    seeds=[
+      b"metadata",
+      token_metadata_program.key().as_ref(),
+      ticket_mint.key().as_ref(),
+      b"edition"
+    ],
+    bump,
+    seeds::program= token_metadata_program.key(),
+  )]
+  /// CHECK: This account check by the metadata smart contract
+  pub ticket_metadata_edition: UncheckedAccount<'info>,
+
+
+  #[account(
+    init,
+    payer=payer,
+    associated_token::mint=ticket_mint,
+    associated_token::authority=payer,
+    associated_token::token_program=token_program,
+  )]
+  pub destination: InterfaceAccount<'info, TokenAccount>,
+
+
+  pub associated_token_program: Program<'info, AssociatedToken>,
+  pub token_metadata_program: Program<'info, Metadata>,
+  pub token_program: Interface<'info, TokenInterface>,
+  pub system_program: Program<'info, System>
 }
 
 #[account]
