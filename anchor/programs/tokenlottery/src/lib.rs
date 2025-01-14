ed29d17afba5ffe3e73f60798bee612bf418ef43
rishavmehra <rishavmehra61@gmail.com>
2025-01-15 01:01:10 +0530

    buy ticket function complete

--- anchor/programs/tokenlottery/src/lib.rs
+++ anchor/programs/tokenlottery/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program;
 use anchor_spl::{
   associated_token::AssociatedToken,
   token_interface::{mint_to, MintTo, TokenInterface, Mint, TokenAccount}
@@ -7,6 +8,8 @@ use anchor_spl::metadata::{
   Metadata,
   create_metadata_accounts_v3,
   create_master_edition_v3,
+  set_and_verify_sized_collection_item,
+  SetAndVerifySizedCollectionItem,
   CreateMetadataAccountsV3,
   CreateMasterEditionV3,
   mpl_token_metadata::types::{
@@ -141,8 +144,113 @@ use super::*;
     Ok(())
   }
 
-  pub fn buy_ticket(_ctx: Context<BuyTicket>)-> Result<()>{
-    ok(())
+  pub fn buy_ticket(ctx: Context<BuyTicket>)-> Result<()>{
+    let clock = Clock::get()?;
+    let ticket_name = NAME.to_owned() + ctx.accounts.token_lottery.total_tickets.to_string().as_str();
+
+    if clock.slot < ctx.accounts.token_lottery.start_time ||
+        clock.slot > ctx.accounts.token_lottery.end_time {
+      return Err(ErrorCode::LotteryNotOpen.into());
+    }
+
+      system_program::transfer(
+        CpiContext::new(
+          ctx.accounts.system_program.to_account_info(), 
+          system_program::Transfer {
+            from: ctx.accounts.payer.to_account_info(),
+            to: ctx.accounts.token_lottery.to_account_info(),
+          }
+        ),
+        ctx.accounts.token_lottery.ticket_price
+      )?;
+
+      ctx.accounts.token_lottery.lottery_pot_amount += ctx.accounts.token_lottery.ticket_price;
+
+      let signer_seeds: &[&[&[u8]]] = &[&[
+        b"collection_mint".as_ref(),
+        &[ctx.bumps.collection_mint],
+      ]];
+
+      mint_to(
+        CpiContext::new_with_signer(
+          ctx.accounts.token_program.to_account_info(), 
+          MintTo{
+            mint: ctx.accounts.collection_mint.to_account_info(),
+            to: ctx.accounts.destination.to_account_info(),
+            authority: ctx.accounts.collection_mint.to_account_info(),
+          }, 
+          signer_seeds
+        ), 
+        1,
+      )?;
+
+      create_metadata_accounts_v3(
+        CpiContext::new_with_signer(
+          ctx.accounts.token_metadata_program.to_account_info(), 
+          CreateMetadataAccountsV3{
+            metadata: ctx.accounts.ticket_metadata.to_account_info(),
+            mint: ctx.accounts.ticket_mint.to_account_info(),
+            mint_authority: ctx.accounts.collection_mint.to_account_info(),
+            payer: ctx.accounts.payer.to_account_info(),
+            update_authority: ctx.accounts.collection_mint.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+          }, 
+          &signer_seeds
+        ), 
+        DataV2{
+          name: ticket_name,
+          symbol: SYMBOL.to_string(),
+          uri: URI.to_string(),
+          seller_fee_basis_points: 0,
+          creators: None,
+          collection: None,
+          uses: None,
+        }, 
+        true, 
+        true, 
+        None
+      )?;
+
+      create_master_edition_v3(
+        CpiContext::new_with_signer(
+          ctx.accounts.token_metadata_program.to_account_info(), 
+          CreateMasterEditionV3{
+            edition: ctx.accounts.ticket_metadata_edition.to_account_info(),
+            mint: ctx.accounts.collection_mint.to_account_info(),
+            update_authority: ctx.accounts.collection_mint.to_account_info(),
+            mint_authority: ctx.accounts.collection_mint.to_account_info(),
+            payer: ctx.accounts.payer.to_account_info(),
+            metadata: ctx.accounts.ticket_metadata.to_account_info(),
+            token_program: ctx.accounts.token_program.to_account_info(),
+            system_program: ctx.accounts.system_program.to_account_info(),
+            rent: ctx.accounts.rent.to_account_info(),
+          }, 
+          &signer_seeds,
+        ), 
+        Some(0),
+      )?;
+
+      set_and_verify_sized_collection_item(
+        CpiContext::new_with_signer(
+          ctx.accounts.token_metadata_program.to_account_info(), 
+          SetAndVerifySizedCollectionItem{
+            metadata: ctx.accounts.ticket_metadata.to_account_info(),
+            collection_authority: ctx.accounts.collection_mint.to_account_info(),
+            payer: ctx.accounts.payer.to_account_info(),
+            update_authority: ctx.accounts.collection_mint.to_account_info(),
+            collection_mint: ctx.accounts.collection_mint.to_account_info(),
+            collection_metadata: ctx.accounts.collection_metadata.to_account_info(),
+            collection_master_edition: ctx.accounts.collection_master_edition.to_account_info(),
+          }, 
+          &signer_seeds
+        ), 
+        None
+      )?;
+
+      ctx.accounts.token_lottery.total_tickets += 1;
+
+      Ok(())
   }
 }
 
@@ -286,6 +394,33 @@ pub struct BuyTicket<'info> {
 
 
   #[account(
+    mut,
+    seeds= [
+      b"metadata",
+      token_metadata_program.key().as_ref(),
+      ticket_mint.key().as_ref(),
+    ],
+    bump,
+    seeds::program= token_metadata_program.key(),
+  )]
+  /// CHECK: This account check by the metadata smart contract
+  pub collection_metadata: UncheckedAccount<'info>,
+
+  #[account(
+    mut, 
+    seeds=[
+      b"metadata",
+      token_metadata_program.key().as_ref(),
+      ticket_mint.key().as_ref(),
+      b"edition"
+    ],
+    bump,
+    seeds::program= token_metadata_program.key(),
+  )]
+  /// CHECK: This account check by the metadata smart contract
+  pub collection_master_edition: UncheckedAccount<'info>,
+
+  #[account(
     init,
     payer=payer,
     associated_token::mint=ticket_mint,
@@ -294,11 +429,11 @@ pub struct BuyTicket<'info> {
   )]
   pub destination: InterfaceAccount<'info, TokenAccount>,
 
-
   pub associated_token_program: Program<'info, AssociatedToken>,
   pub token_metadata_program: Program<'info, Metadata>,
   pub token_program: Interface<'info, TokenInterface>,
-  pub system_program: Program<'info, System>
+  pub system_program: Program<'info, System>,
+  pub rent: Sysvar<'info, Rent>
 }
 
 #[account]
@@ -314,4 +449,10 @@ pub struct TokenLottery{
   pub authority: Pubkey,
   pub randomness_account: Pubkey,
   pub bump: u8,
+}
+
+#[error_code]
+pub enum ErrorCode {
+  #[msg("Lottery iis not open")]
+  LotteryNotOpen,
 }
